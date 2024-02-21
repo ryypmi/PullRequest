7e4a4cc06fb2402e63cc569c89825ada4d155037
Mikko Ryyppö <fmiry001@edu.xamk.fi>
2024-02-21 13:17:54 +0200

    Add project files.

---
+++ PullRequest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PullRequest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
